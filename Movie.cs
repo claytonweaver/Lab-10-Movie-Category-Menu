1cde656119936658ecd3c1db91a88e2d336c398c
claytonweaver <cweaver@cbctrenton.com>
2020-01-30 19:24:23 -0500

    lab 10 Movie Category V.1

---
+++ Movie.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_10_Movie_List
+{
+    class Movie
+    {
+        private string title;
+        private string category;
+        private int categoryNumber;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+        public int CategoryNumber
+        {
+            get { return categoryNumber; }
+            set { categoryNumber = value; } 
+        }
+        public Movie()
+        {
+
+        }
+
+        public Movie(string _title, string _category, int _categoryNumber)
+        {
+            title = _title;
+            category = _category;
+            categoryNumber = _categoryNumber;
+        }
+
+        private static List<string> GetListofMovieCategories(List<Movie> movieList)
+        {
+            List<string> movieCategories = new List<string>();
+            foreach (var movie in movieList)
+            {
+                if (!movieCategories.Contains(movie.category))
+                {
+                    movieCategories.Add(movie.category);
+                }
+            }
+            return movieCategories;
+        }
+
+        public static void ShowMoviesByCategories(List<Movie> movies, int userNumber)
+        {
+            foreach (var movie in movies)
+            {
+                if (userNumber == movie.categoryNumber)
+                {
+                    Console.WriteLine($"{movie.title} - {movie.category}");
+                }
+            }
+        }
+        public static void ShowMovieMenu(List<Movie> movies)
+        {
+            for (int i = 0; i < movies.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {movies[i].title} ({movies[i].category})");
+
+            }
+        }
+        public static void ShowCategoryMenu(List<Movie> movies)
+        {
+            List<string> movieCategories = new List<string>(GetListofMovieCategories(movies));
+            for (int i = 1; i < movieCategories.Count + 1; i++)
+            {
+                Console.Write($"{i}) {movieCategories[i-1]} ");
+               
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_10_Movie_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> movies = new List<Movie>
+            {
+                new Movie("Star Wars","sci-fi", 1),
+                new Movie("Pulp Fiction","action", 2),
+                new Movie("Fight Club", "thriller", 3),
+                new Movie("Scary Movie 3", "comedy", 4),
+                new Movie("Indiana Jones","action", 2),
+                new Movie("Star Trek","sci-fi", 1),
+                new Movie("Airplane","comedy", 4)
+            };
+            bool resume = false; 
+            do
+            {
+                try
+                {
+                    Movie.ShowMovieMenu(movies);
+                    Console.WriteLine();
+                    Movie.ShowCategoryMenu(movies);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    int userChoice = int.Parse(GetUserInput("Which category would you like to see?"));
+                    Movie.ShowMoviesByCategories(movies, userChoice);
+                    resume = AskToTryAgain(GetUserInput("Would you like to try again? (y/n)"));
+
+                    
+                }
+                catch (FormatException)
+                {
+                    resume = AskToTryAgain(GetUserInput("Wrong input. Try Again? (y/n)"));
+                }
+                
+            } while (resume == true);
+            
+
+
+        }
+
+        static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        static bool AskToTryAgain(string input)
+        {
+            try
+            {
+                if (input.ToLower()[0] == 'y')
+                {
+                    return true;
+                }
+                else if (input.ToLower()[0] == 'n')
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input. Would you like to try again? (y/n)");
+                    string input2 = Console.ReadLine();
+                    AskToTryAgain(input2);
+                }
+
+            }
+            catch (StackOverflowException)
+            {
+
+                string userError = GetUserInput("That's not right. Try again: 'y' or 'n'");
+                AskToTryAgain(userError);
+            }
+
+            return true;
+
+        }
+
+
+
+
+
+    }
+}
